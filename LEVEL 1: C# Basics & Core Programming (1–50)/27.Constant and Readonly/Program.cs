00941bbfc1d8a664d064d4b45a89814c1ca43a87
Kishorpatil007 <kp675541@gmail.com>
2026-03-05 12:03:58 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/27.Constant and Readonly/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    const double pi = 3.14;
+    readonly int number;
+
+    public Program(int n)
+    {
+        number = n;
+    }
+
+    static void Main()
+    {
+        Program obj = new Program(100);
+        Console.WriteLine("Constant PI: " + pi);
+        Console.WriteLine("Readonly Number: " + obj.number);
+    }
+}
