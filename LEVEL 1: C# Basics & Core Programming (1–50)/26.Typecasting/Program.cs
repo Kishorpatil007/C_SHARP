b00b4703a8def7122cce99b938a2122d953beedb
Kishorpatil007 <kp675541@gmail.com>
2026-03-05 12:01:13 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/26.Typecasting/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter an integer: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        double b = a;
+        Console.WriteLine("Implicit Casting to double: " + b);
+
+        double x = 45.67;
+        int y = (int)x;
+        Console.WriteLine("Explicit Casting to int: " + y);
+    }
+}
