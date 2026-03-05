11d2ed83a1e423d168938dad028a3b2e911c5553
Kishorpatil007 <kp675541@gmail.com>
2026-03-05 19:32:19 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/30.Switch Case/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number (1-3): ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            default:
+                Console.WriteLine("Invalid Choice");
+                break;
+        }
+    }
+}
