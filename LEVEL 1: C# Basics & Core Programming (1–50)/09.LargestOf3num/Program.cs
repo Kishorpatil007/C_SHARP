4202facd4fc9792823a402d31e4331719f047271
Kishorpatil007 <kp675541@gmail.com>
2026-03-05 11:21:08 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/09.LargestOf3num/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int max = a;
+
+        if (b > max)
+            max = b;
+        if (c > max)
+            max = c;
+
+        Console.WriteLine("Largest: " + max);
+    }
+}
