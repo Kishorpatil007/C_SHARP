55857c5abe0b8480ff53cac0a7493985e1b24440
Kishorpatil007 <kp675541@gmail.com>
2026-03-05 11:25:59 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/12.Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of terms: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int first = 0, second = 1, next;
+
+        Console.WriteLine("Fibonacci Series:");
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write(first + " ");
+
+            next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}
