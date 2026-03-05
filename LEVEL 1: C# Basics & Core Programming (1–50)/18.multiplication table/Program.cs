aa85fc76f51cec17625b4796cbcbc585a6f0e408
Kishorpatil007 <kp675541@gmail.com>
2026-03-05 11:35:16 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/18.multiplication table/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Multiplication Table of " + num);
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
