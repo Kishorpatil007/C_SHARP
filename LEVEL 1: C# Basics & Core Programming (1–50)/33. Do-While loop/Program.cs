036504a13b2bf80d997fc1dbb7befe1f2327b133
Kishorpatil007 <kp675541@gmail.com>
2026-03-05 19:37:09 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/33. Do-While loop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int i = 1;
+
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i <= n);
+    }
+}
