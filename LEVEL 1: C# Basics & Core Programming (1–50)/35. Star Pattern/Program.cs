ee2895794d9827a0b05eae118479e58f0685c6a7
Kishorpatil007 <kp675541@gmail.com>
2026-03-05 19:39:15 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/35. Star Pattern/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of rows: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
