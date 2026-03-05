52db72226fc50ca291f7d81df8fa18afe306b38c
Kishorpatil007 <kp675541@gmail.com>
2026-03-05 11:36:54 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/19.Prime number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter starting number: ");
+        int start = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter ending number: ");
+        int end = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Prime numbers between " + start + " and " + end + ":");
+
+        for (int i = start; i <= end; i++)
+        {
+            int count = 0;
+
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                    count++;
+            }
+
+            if (count == 2)
+                Console.Write(i + " ");
+        }
+    }
+}
