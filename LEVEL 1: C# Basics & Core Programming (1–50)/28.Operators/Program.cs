92ebe20799a7d550c5b7f6d6a1bb80a3b251ccf3
Kishorpatil007 <kp675541@gmail.com>
2026-03-05 19:29:42 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/28.Operators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Addition: " + (a + b));
+        Console.WriteLine("Subtraction: " + (a - b));
+        Console.WriteLine("Multiplication: " + (a * b));
+        Console.WriteLine("Division: " + (a / b));
+        Console.WriteLine("Modulus: " + (a % b));
+    }
+}
