48ca4d0622c2f19aeb50b1d910a9a6a6e05d619a
Kishorpatil007 <kp675541@gmail.com>
2026-03-05 11:18:39 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/07.Simple Interest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter Principal: ");
+        int p = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Rate: ");
+        int r = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Time: ");
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        int si = (p * r * t) / 100;
+
+        Console.WriteLine("Simple Interest: " + si);
+    }
+}
