e4b6ea57d7d98cdfc26220e3b584a35f4b56368d
Kishorpatil007 <kp675541@gmail.com>
2026-03-06 10:42:34 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/39.Calculate Salary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter basic salary: ");
+        double basic = Convert.ToDouble(Console.ReadLine());
+
+        double hra = basic * 0.20;
+        double da = basic * 0.10;
+        double total = basic + hra + da;
+
+        Console.WriteLine("HRA: " + hra);
+        Console.WriteLine("DA: " + da);
+        Console.WriteLine("Total Salary: " + total);
+    }
+}
