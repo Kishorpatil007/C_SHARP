47968d43db9aeb75a6f9567254aa613973858ef7
Kishorpatil007 <kp675541@gmail.com>
2026-03-06 10:39:31 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/37.Power of number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter base number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter power: ");
+        int power = Convert.ToInt32(Console.ReadLine());
+
+        double result = Math.Pow(num, power);
+
+        Console.WriteLine("Result: " + result);
+    }
+}
