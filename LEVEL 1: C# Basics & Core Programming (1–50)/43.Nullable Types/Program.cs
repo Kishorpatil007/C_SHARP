29ea5a7ab43f43924c1b6d3ad6584e04141bf346
Kishorpatil007 <kp675541@gmail.com>
2026-03-06 10:47:07 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/43.Nullable Types/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class NullableDemo
+{
+    static void Main()
+    {
+        int? num = null;
+
+        if (num.HasValue)
+            Console.WriteLine("Value: " + num.Value);
+        else
+            Console.WriteLine("Value is Null");
+
+        num = 50;
+
+        if (num.HasValue)
+            Console.WriteLine("Value: " + num.Value);
+        else
+            Console.WriteLine("Value is Null");
+    }
+}
