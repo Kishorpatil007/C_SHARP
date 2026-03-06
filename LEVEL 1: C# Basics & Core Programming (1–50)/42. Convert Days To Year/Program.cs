be94bd28819aedb8dfefe1704cc99225242464d8
Kishorpatil007 <kp675541@gmail.com>
2026-03-06 10:46:13 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/42. Convert Days To Year/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class DaysConvert
+{
+    static void Main()
+    {
+        Console.Write("Enter number of days: ");
+        int days = int.Parse(Console.ReadLine());
+
+        int years = days / 365;
+        days = days % 365;
+        int months = days / 30;
+        days = days % 30;
+
+        Console.WriteLine("Years: " + years);
+        Console.WriteLine("Months: " + months);
+        Console.WriteLine("Days: " + days);
+    }
+}
