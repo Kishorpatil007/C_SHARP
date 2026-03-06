e6be48e8356ffa09998c94fc5e862aa8d87b9075
Kishorpatil007 <kp675541@gmail.com>
2026-03-06 10:38:09 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/36.Random number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random r = new Random();
+
+        Console.Write("How many random numbers: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(r.Next(1, 101));
+        }
+    }
+}
