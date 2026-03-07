0884f8b7442a91cc66ddb801f60b8aa977bbdf07
Kishorpatil007 <kp675541@gmail.com>
2026-03-07 19:06:47 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/47.Tuples/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class TupleDemo
+{
+    static void Main()
+    {
+        var person = (Id: 20, Name: "Kishor", Age: 22);
+
+        Console.WriteLine("Id: " + person.Id);
+        Console.WriteLine("Name: " + person.Name);
+        Console.WriteLine("Age: " + person.Age);
+    }
+}
