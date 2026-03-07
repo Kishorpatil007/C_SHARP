fbbb88ab452f5202ce3ef26a4bd3e44db5be1b82
Kishorpatil007 <kp675541@gmail.com>
2026-03-07 19:16:05 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/50.Date Time Class/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class DateTimeDemo
+{
+    static void Main()
+    {
+        DateTime now = DateTime.Now;
+
+        Console.WriteLine("Current Date: " + now.ToShortDateString());
+        Console.WriteLine("Current Time: " + now.ToShortTimeString());
+        Console.WriteLine("Day: " + now.Day);
+        Console.WriteLine("Month: " + now.Month);
+        Console.WriteLine("Year: " + now.Year);
+    }
+}
