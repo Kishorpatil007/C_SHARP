ead4e0be7a53e19be6ad6cadb6f354a925fc4bd3
Kishorpatil007 <kp675541@gmail.com>
2026-03-07 19:04:37 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/45.Enum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class EnumDemo
+{
+    enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday }
+
+    static void Main()
+    {
+        Console.Write("Enter number (0-6): ");
+        int n = int.Parse(Console.ReadLine());
+
+        Days day = (Days)n;
+        Console.WriteLine("Day is: " + day);
+    }
+}
