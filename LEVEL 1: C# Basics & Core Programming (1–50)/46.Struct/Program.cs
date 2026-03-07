88b89bdaeda966a27ede0aca6db641f30974676c
Kishorpatil007 <kp675541@gmail.com>
2026-03-07 19:05:50 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/46.Struct/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+struct Student
+{
+    public int Id;
+    public string Name;
+
+    public void Display()
+    {
+        Console.WriteLine("Id: " + Id);
+        Console.WriteLine("Name: " + Name);
+    }
+}
+
+class StructDemo
+{
+    static void Main()
+    {
+        Student s;
+        s.Id = 20;
+        s.Name = "Kishor";
+        s.Display();
+    }
+}
