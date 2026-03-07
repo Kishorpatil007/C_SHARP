9ecf420cf5f92ac9416f14a835cc6eb0bdaeb625
Kishorpatil007 <kp675541@gmail.com>
2026-03-07 19:07:46 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/48. String Interpolation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class StringInterpolationDemo
+{
+    static void Main()
+    {
+        string name = "Kishor";
+        int age = 22;
+
+        Console.WriteLine($"My name is {name} and I am {age} years old.");
+    }
+}
