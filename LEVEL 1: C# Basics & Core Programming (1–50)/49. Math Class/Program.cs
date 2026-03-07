93565eb25998a4d8b0c7bf848db5aa3aa0c5de25
Kishorpatil007 <kp675541@gmail.com>
2026-03-07 19:12:18 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/49. Math Class/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MathDemo
+{
+    static void Main()
+    {
+        double a = 25;
+        double b = 4;
+
+        Console.WriteLine("Square Root: " + Math.Sqrt(a));
+        Console.WriteLine("Power: " + Math.Pow(a, 2));
+        Console.WriteLine("Maximum: " + Math.Max(a, b));
+        Console.WriteLine("Minimum: " + Math.Min(a, b));
+    }
+}
