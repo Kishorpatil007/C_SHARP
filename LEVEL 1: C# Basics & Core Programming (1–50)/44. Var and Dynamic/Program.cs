c672a6d297e96401777b3eef85e55cd0ebdaad5a
Kishorpatil007 <kp675541@gmail.com>
2026-03-07 19:03:44 +0530

    Add files via upload

---
+++ LEVEL 1: C# Basics & Core Programming (1–50)/44. Var and Dynamic/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class VarDynamicDemo
+{
+    static void Main()
+    {
+        var a = 10;
+        var b = "Hello";
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        dynamic x = 20;
+        Console.WriteLine(x);
+
+        x = "World";
+        Console.WriteLine(x);
+    }
+}
