326860ca0a5d7035b1c1f066c78dbf455ce3274e
Kishorpatil007 <kp675541@gmail.com>
2026-03-18 12:06:59 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/59.Perform Matrix Addition/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter rows and columns: ");
+        int r = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int[,] a = new int[r, c];
+        int[,] b = new int[r, c];
+        int[,] sum = new int[r, c];
+
+        Console.WriteLine("Enter first matrix:");
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                a[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        Console.WriteLine("Enter second matrix:");
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                b[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                sum[i, j] = a[i, j] + b[i, j];
+            }
+        }
+
+        Console.WriteLine("Sum matrix:");
+        for (int i = 0; i < r; i++)
+        {
+            for (int j = 0; j < c; j++)
+            {
+                Console.Write(sum[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
