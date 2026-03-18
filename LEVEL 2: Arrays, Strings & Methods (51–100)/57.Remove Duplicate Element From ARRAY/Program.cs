42098ac5903ea227c95f84aad652422ae694103d
Kishorpatil007 <kp675541@gmail.com>
2026-03-18 12:01:27 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/57.Remove Duplicate Element From ARRAY/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter elements:");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Array after removing duplicates:");
+        for (int i = 0; i < n; i++)
+        {
+            bool isDuplicate = false;
+            for (int j = 0; j < i; j++)
+            {
+                if (arr[i] == arr[j])
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate)
+                Console.Write(arr[i] + " ");
+        }
+    }
+}
