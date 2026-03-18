4cc28ef0088502fc383392fae6b974031e26f409
Kishorpatil007 <kp675541@gmail.com>
2026-03-18 12:11:23 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/61.transpose of matrix/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+        int count = 0;
+
+        foreach (char ch in str.ToLower())
+        {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Number of vowels = " + count);
+    }
+}
