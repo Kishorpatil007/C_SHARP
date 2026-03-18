2da5f6ba37065075f7e6ae138c49423b636654e5
Kishorpatil007 <kp675541@gmail.com>
2026-03-18 11:42:45 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/52.Sum and avg of array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+        int sum = 0;
+
+        Console.WriteLine("Enter elements:");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+            sum += arr[i];
+        }
+
+        double avg = (double)sum / n;
+
+        Console.WriteLine("Sum = " + sum);
+        Console.WriteLine("Average = " + avg);
+    }
+}
