8dfd73507b92f4f2103e868aa163e01677a6a878
Kishorpatil007 <kp675541@gmail.com>
2026-03-18 12:04:02 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/58.Search An element in array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter elements:");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.Write("Enter element to search: ");
+        int key = Convert.ToInt32(Console.ReadLine());
+        bool found = false;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == key)
+            {
+                Console.WriteLine("Element found at position " + (i + 1));
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+            Console.WriteLine("Element not found");
+    }
+}
