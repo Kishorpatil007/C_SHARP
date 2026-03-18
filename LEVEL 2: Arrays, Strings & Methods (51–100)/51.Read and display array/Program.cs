399faa9046da441f43e453faf5ca59b29df8fa09
Kishorpatil007 <kp675541@gmail.com>
2026-03-18 11:41:13 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/51.Read and display array/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter number of elements: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter elements:");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        Console.WriteLine("Array elements are:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    }
+}
