47330d8563940816e40291089d64cdf7d4d762b9
Kishorpatil007 <kp675541@gmail.com>
2026-03-26 21:52:15 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/63.Reverse String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a string: ");
+        string str = Console.ReadLine();
+        string rev = "";
+
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            rev += str[i];
+        }
+
+        Console.WriteLine("Reversed string = " + rev);
+    }
+}
