f11aff46cbad5ecadf4378c898e05d21854cb3af
Kishorpatil007 <kp675541@gmail.com>
2026-04-03 06:57:10 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/65.COUNT_WORDS_IN_STRING/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = Console.ReadLine();
+        string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        Console.WriteLine(words.Length);
+    }
+}
