d5c1a4d96f74ba8517812a9c8b91e02f82039c1e
Kishorpatil007 <kp675541@gmail.com>
2026-04-03 07:04:56 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/70.Comparing String/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str1 = Console.ReadLine();
+        string str2 = Console.ReadLine();
+
+        if (str1.Equals(str2))
+            Console.WriteLine("Equal");
+        else
+            Console.WriteLine("Not Equal");
+
+        int result = string.Compare(str1, str2);
+        Console.WriteLine(result);
+    }
+}
