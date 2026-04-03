45acc7f11e98264ff264d1aebbff0d76ef07a7ab
Kishorpatil007 <kp675541@gmail.com>
2026-04-03 07:03:19 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/68.repalce substring/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = Console.ReadLine();
+        string oldWord = Console.ReadLine();
+        string newWord = Console.ReadLine();
+        string result = str.Replace(oldWord, newWord);
+        Console.WriteLine(result);
+    }
+}
