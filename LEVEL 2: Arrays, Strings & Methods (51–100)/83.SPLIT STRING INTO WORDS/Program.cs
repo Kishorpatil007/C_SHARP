b725d4d5b4db0cae630eb25cb75e647f2ad7b284
Kishorpatil007 <kp675541@gmail.com>
2026-04-05 04:51:29 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/83.SPLIT STRING INTO WORDS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "Hello world from CSharp";
+        string[] words = str.Split(' ');
+
+        foreach (string word in words)
+            Console.WriteLine(word);
+    }
+}
