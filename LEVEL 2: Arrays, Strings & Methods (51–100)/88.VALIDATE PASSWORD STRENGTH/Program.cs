6e1950581d7789991d95456857e7f956489cafe9
Kishorpatil007 <kp675541@gmail.com>
2026-04-05 04:56:32 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/88.VALIDATE PASSWORD STRENGTH/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string password = "Abc@1234";
+        bool isValid = Regex.IsMatch(password, @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}$");
+
+        Console.WriteLine(isValid ? "Strong Password" : "Weak Password");
+    }
+}
