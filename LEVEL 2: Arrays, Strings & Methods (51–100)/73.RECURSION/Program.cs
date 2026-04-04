f2a0cee10b75b1da452c07e8877c0674ff37819c
Kishorpatil007 <kp675541@gmail.com>
2026-04-04 19:43:15 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/73.RECURSION/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void PrintNumbers(int n)
+    {
+        if (n == 0) return;
+        Console.Write(n + " ");
+        PrintNumbers(n - 1);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int n = int.Parse(Console.ReadLine());
+        PrintNumbers(n);
+    }
+}
