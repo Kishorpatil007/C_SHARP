ee243fd23c88537cdaf50c41e0773bd1fde16e9b
Kishorpatil007 <kp675541@gmail.com>
2026-04-05 04:58:33 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/90.DENONSTRATE INDEXERS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Sample
+{
+    private int[] arr = new int[5];
+
+    public int this[int index]
+    {
+        get { return arr[index]; }
+        set { arr[index] = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Sample s = new Sample();
+
+        for (int i = 0; i < 5; i++)
+            s[i] = i * 10;
+
+        for (int i = 0; i < 5; i++)
+            Console.Write(s[i] + " ");
+    }
+}
