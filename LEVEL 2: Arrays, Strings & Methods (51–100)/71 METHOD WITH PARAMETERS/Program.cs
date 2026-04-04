e2e8794f3b04938c4028afdc20a91dccb31e0d42
Kishorpatil007 <kp675541@gmail.com>
2026-04-04 19:23:32 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/71 METHOD WITH PARAMETERS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Add(int a, int b)
+    {
+        Console.WriteLine(a + b);
+    }
+
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        Add(x, y);
+    }
+}
