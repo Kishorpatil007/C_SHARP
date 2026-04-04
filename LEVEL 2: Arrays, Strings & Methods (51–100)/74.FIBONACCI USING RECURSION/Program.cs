a56e38441a8bc51d0bca273e3eaf511c4ba9ecdd
Kishorpatil007 <kp675541@gmail.com>
2026-04-04 19:44:15 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/74.FIBONACCI USING RECURSION/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static int Fib(int n)
+    {
+        if (n <= 1) return n;
+        return Fib(n - 1) + Fib(n - 2);
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter limit: ");
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+            Console.Write(Fib(i) + " ");
+    }
+}
