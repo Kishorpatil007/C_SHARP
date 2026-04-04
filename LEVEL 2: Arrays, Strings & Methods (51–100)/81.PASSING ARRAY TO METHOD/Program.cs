191fe58ad805e7688201dbd249cf75e80a23e55c
Kishorpatil007 <kp675541@gmail.com>
2026-04-05 04:49:23 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/81.PASSING ARRAY TO METHOD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void PrintArray(int[] arr)
+    {
+        foreach (int i in arr)
+            Console.Write(i + " ");
+    }
+
+    static void Main()
+    {
+        int[] arr = { 10, 20, 30, 40 };
+        PrintArray(arr);
+    }
+}
