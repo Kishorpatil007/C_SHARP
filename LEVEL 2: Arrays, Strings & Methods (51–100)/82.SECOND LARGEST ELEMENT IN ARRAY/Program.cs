cec616d8bb292d093363d0b1ae800ce534c9cb7f
Kishorpatil007 <kp675541@gmail.com>
2026-04-05 04:50:20 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/82.SECOND LARGEST ELEMENT IN ARRAY/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 12, 35, 1, 10, 34, 1 };
+        int largest = int.MinValue, second = int.MinValue;
+
+        foreach (int num in arr)
+        {
+            if (num > largest)
+            {
+                second = largest;
+                largest = num;
+            }
+            else if (num > second && num != largest)
+            {
+                second = num;
+            }
+        }
+
+        Console.WriteLine(second);
+    }
+}
