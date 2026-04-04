600a7aab7aa8a7a90df6a88d8c21a8356e642700
Kishorpatil007 <kp675541@gmail.com>
2026-04-05 04:52:32 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/84.CHECK ANAGRAM STRING/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str1 = "listen";
+        string str2 = "silent";
+
+        char[] a = str1.ToLower().ToCharArray();
+        char[] b = str2.ToLower().ToCharArray();
+
+        Array.Sort(a);
+        Array.Sort(b);
+
+        string s1 = new string(a);
+        string s2 = new string(b);
+
+        if (s1 == s2)
+            Console.WriteLine("Anagram");
+        else
+            Console.WriteLine("Not Anagram");
+    }
+}
