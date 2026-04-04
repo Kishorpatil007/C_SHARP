f68e00bdd6fd1535c047c375feef39277399dbea
Kishorpatil007 <kp675541@gmail.com>
2026-04-04 19:46:03 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/76.OUT AND REF KEY/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void UsingOut(out int x)
+    {
+        x = 50;
+    }
+
+    static void UsingRef(ref int y)
+    {
+        y += 10;
+    }
+
+    static void Main()
+    {
+        int a;
+        UsingOut(out a);
+        Console.WriteLine(a);
+
+        int b = 20;
+        UsingRef(ref b);
+        Console.WriteLine(b);
+    }
+}
