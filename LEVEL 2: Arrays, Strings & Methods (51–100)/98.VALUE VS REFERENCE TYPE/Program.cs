8e5af2c9ed79ee3b9045150d610c577377195f0a
Kishorpatil007 <kp675541@gmail.com>
2026-04-06 20:03:05 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/98.VALUE VS REFERENCE TYPE/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Demo
+{
+    public int value;
+}
+
+class Program
+{
+    static void Main()
+    {
+        int a = 10;
+        int b = a;
+        b = 20;
+
+        Console.WriteLine("Value Types:");
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+
+        Demo obj1 = new Demo();
+        obj1.value = 10;
+
+        Demo obj2 = obj1;
+        obj2.value = 20;
+
+        Console.WriteLine("Reference Types:");
+        Console.WriteLine(obj1.value);
+        Console.WriteLine(obj2.value);
+    }
+}
