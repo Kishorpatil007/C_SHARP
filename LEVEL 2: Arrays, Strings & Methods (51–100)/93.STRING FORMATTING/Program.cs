e637a6de4dae9114d832063091135a2300a52aac
Kishorpatil007 <kp675541@gmail.com>
2026-04-06 19:58:04 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/93.STRING FORMATTING/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string name = "Kishor";
+        int age = 20;
+        double marks = 85.75;
+
+        Console.WriteLine("Name: {0}, Age: {1}, Marks: {2:F2}", name, age, marks);
+        Console.WriteLine($"Name: {name}, Age: {age}, Marks: {marks}");
+    }
+}
