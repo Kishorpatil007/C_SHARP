bde2a462ae2cd9cbd3d321eb2ddaf65e85b13a7b
Kishorpatil007 <kp675541@gmail.com>
2026-04-06 20:02:09 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/97.SPAN AND READ ONLY SPAN/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        Span<int> span = numbers;
+        span[1] = 99;
+
+        ReadOnlySpan<int> readOnlySpan = numbers;
+
+        foreach (var n in span)
+        {
+            Console.Write(n + " ");
+        }
+
+        Console.WriteLine();
+
+        foreach (var n in readOnlySpan)
+        {
+            Console.Write(n + " ");
+        }
+    }
+}
