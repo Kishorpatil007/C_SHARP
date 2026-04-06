e532049724d2b0fc8f3ac31fd3ab44d64a0e934a
Kishorpatil007 <kp675541@gmail.com>
2026-04-06 20:00:09 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/95.FIND MISSING NUMBER IN ARRAYS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = { 1, 2, 4, 5 };
+        int n = 5;
+
+        int total = n * (n + 1) / 2;
+        int sum = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+
+        int missing = total - sum;
+
+        Console.WriteLine("Missing Number: " + missing);
+    }
+}
