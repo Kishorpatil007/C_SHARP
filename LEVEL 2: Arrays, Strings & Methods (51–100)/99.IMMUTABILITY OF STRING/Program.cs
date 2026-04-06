65b7b0647830eb612697416786c86ecb4d396f76
Kishorpatil007 <kp675541@gmail.com>
2026-04-06 20:03:58 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/99.IMMUTABILITY OF STRING/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str = "Hello";
+        string newStr = str;
+
+        newStr = newStr + " World";
+
+        Console.WriteLine(str);
+        Console.WriteLine(newStr);
+    }
+}
