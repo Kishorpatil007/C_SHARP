c9bd77d9a8cf67bd89e8a3d3827116ba5198f4ff
Kishorpatil007 <kp675541@gmail.com>
2026-04-06 20:05:06 +0530

    Add files via upload

---
+++ LEVEL 2: Arrays, Strings & Methods (51–100)/100.MEMEORY MANAGEMENT BASICS/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Demo
+{
+    public Demo()
+    {
+        Console.WriteLine("Object Created");
+    }
+
+    ~Demo()
+    {
+        Console.WriteLine("Object Destroyed");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Demo d1 = new Demo();
+        Demo d2 = new Demo();
+
+        d1 = null;
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+
+        Console.WriteLine("End of Main");
+    }
+}
