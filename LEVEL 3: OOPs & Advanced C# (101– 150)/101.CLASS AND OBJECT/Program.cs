893fd5a117ae96e40933e397a16d3511efc98947
Kishorpatil007 <kp675541@gmail.com>
2026-04-07 20:28:46 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/101.CLASS AND OBJECT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Student
+{
+    public string name;
+    public int age;
+
+    public void display()
+    {
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student();
+        s.name = "Kishor";
+        s.age = 20;
+        s.display();
+    }
+}
