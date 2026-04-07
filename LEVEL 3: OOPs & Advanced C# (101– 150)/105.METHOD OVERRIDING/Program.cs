2e82276fe16227bda9fa2e6bc8bcea5066666694
Kishorpatil007 <kp675541@gmail.com>
2026-04-07 20:33:10 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/105.METHOD OVERRIDING/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Parent
+{
+    public virtual void show()
+    {
+        Console.WriteLine("Parent Method");
+    }
+}
+
+class Child : Parent
+{
+    public override void show()
+    {
+        Console.WriteLine("Child Method");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Parent p = new Child();
+        p.show();
+    }
+}
