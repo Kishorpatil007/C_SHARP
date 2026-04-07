1a9c22eadaec9714d43c2e25654375103d441c78
Kishorpatil007 <kp675541@gmail.com>
2026-04-07 20:34:16 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/106.POLYMORPHISM/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Demo
+{
+    public void add(int a, int b)
+    {
+        Console.WriteLine(a + b);
+    }
+
+    public void add(double a, double b)
+    {
+        Console.WriteLine(a + b);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Demo d = new Demo();
+        d.add(10, 20);
+        d.add(5.5, 2.5);
+    }
+}
