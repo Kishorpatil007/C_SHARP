7b9d89fd194b8442875bec906f11af4f0248e1f3
Kishorpatil007 <kp675541@gmail.com>
2026-04-07 20:31:56 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/104.INHERITANCE/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Parent
+{
+    public void show()
+    {
+        Console.WriteLine("Parent Class");
+    }
+}
+
+class Child : Parent
+{
+    public void display()
+    {
+        Console.WriteLine("Child Class");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Child c = new Child();
+        c.show();
+        c.display();
+    }
+}
