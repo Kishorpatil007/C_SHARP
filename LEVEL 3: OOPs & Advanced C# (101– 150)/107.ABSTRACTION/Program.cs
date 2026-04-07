65ff0825755461d6e411a79db5566019cdb4545f
Kishorpatil007 <kp675541@gmail.com>
2026-04-07 20:35:11 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/107.ABSTRACTION/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract void draw();
+}
+
+class Circle : Shape
+{
+    public override void draw()
+    {
+        Console.WriteLine("Drawing Circle");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Shape s = new Circle();
+        s.draw();
+    }
+}
