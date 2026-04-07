3948b3cd3d48f57a9f2fd9135ad8d09e0ebb1678
Kishorpatil007 <kp675541@gmail.com>
2026-04-07 20:37:29 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/109.MULTIPLE INHERITANCE/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IA
+{
+    void showA();
+}
+
+interface IB
+{
+    void showB();
+}
+
+class Demo : IA, IB
+{
+    public void showA()
+    {
+        Console.WriteLine("Interface A");
+    }
+
+    public void showB()
+    {
+        Console.WriteLine("Interface B");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Demo d = new Demo();
+        d.showA();
+        d.showB();
+    }
+}
