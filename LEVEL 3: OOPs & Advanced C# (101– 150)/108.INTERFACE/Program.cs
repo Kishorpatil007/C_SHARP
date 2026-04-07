3cd2abf819851011907944ab9bf438d6f7400e05
Kishorpatil007 <kp675541@gmail.com>
2026-04-07 20:36:19 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/108.INTERFACE/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+interface IAnimal
+{
+    void sound();
+}
+
+class Dog : IAnimal
+{
+    public void sound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IAnimal a = new Dog();
+        a.sound();
+    }
+}
