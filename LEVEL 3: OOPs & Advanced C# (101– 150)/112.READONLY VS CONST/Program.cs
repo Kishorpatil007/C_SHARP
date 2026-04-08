eeba5cd2f4b056c0a4c5c14a0959a13b185dbd4e
Kishorpatil007 <kp675541@gmail.com>
2026-04-08 19:29:18 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/112.READONLY VS CONST/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Demo
+{
+    public readonly int x;
+    public const int y = 50;
+
+    public Demo(int val)
+    {
+        x = val;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter value: ");
+        int val = int.Parse(Console.ReadLine());
+
+        Demo d = new Demo(val);
+        d.Show();
+    }
+}
