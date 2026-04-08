17c26447840570057d6dd8d0a6fc2e5d8ce70706
Kishorpatil007 <kp675541@gmail.com>
2026-04-08 19:30:46 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/113.STATIC CLASS/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+static class Demo
+{
+    public static int a = 10;
+
+    public static void Show()
+    {
+        Console.WriteLine(a);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Demo.Show();
+    }
+}
