61b84248fc5fd7375d900c66bd2ed3e2eecfe5ed
Kishorpatil007 <kp675541@gmail.com>
2026-04-09 21:46:40 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/118.DEPENDENCY INJECTION/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+interface IMessage
+{
+    void SendMessage(string message);
+}
+
+class EmailMessage : IMessage
+{
+    public void SendMessage(string message)
+    {
+        Console.WriteLine("Email sent: " + message);
+    }
+}
+
+class Notification
+{
+    private IMessage _messageService;
+
+    public Notification(IMessage messageService)
+    {
+        _messageService = messageService;
+    }
+
+    public void Notify(string message)
+    {
+        _messageService.SendMessage(message);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IMessage email = new EmailMessage();
+        Notification notification = new Notification(email);
+        notification.Notify("Hello, Kishor!");
+    }
+}
