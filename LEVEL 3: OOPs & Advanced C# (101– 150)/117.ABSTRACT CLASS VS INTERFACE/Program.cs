92975439d898955932934ffa70ceef040824978e
Kishorpatil007 <kp675541@gmail.com>
2026-04-09 21:45:43 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/117.ABSTRACT CLASS VS INTERFACE/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract void Draw();
+}
+
+interface IColor
+{
+    void Fill();
+}
+
+class Circle : Shape, IColor
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing Circle");
+    }
+
+    public void Fill()
+    {
+        Console.WriteLine("Filling Circle with color");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Circle c = new Circle();
+        c.Draw();
+        c.Fill();
+    }
+}
