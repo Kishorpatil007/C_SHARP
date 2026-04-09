c91a6d177668d07d49c821064a60d7800f588f11
Kishorpatil007 <kp675541@gmail.com>
2026-04-09 21:47:26 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/119.SOLID PRINCIPLES/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+interface IWorker
+{
+    void Work();
+}
+
+class Developer : IWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("Developer writing code");
+    }
+}
+
+class Manager : IWorker
+{
+    public void Work()
+    {
+        Console.WriteLine("Manager managing team");
+    }
+}
+
+class WorkManager
+{
+    public void Manage(IWorker worker)
+    {
+        worker.Work();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IWorker dev = new Developer();
+        IWorker mgr = new Manager();
+
+        WorkManager manager = new WorkManager();
+        manager.Manage(dev);
+        manager.Manage(mgr);
+    }
+}
