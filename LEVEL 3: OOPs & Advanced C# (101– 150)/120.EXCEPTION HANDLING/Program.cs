57e6e1bbd52380105f0e22c492e89c65c0550a34
Kishorpatil007 <kp675541@gmail.com>
2026-04-09 21:48:29 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/120.EXCEPTION HANDLING/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number:");
+        try
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            int result = 100 / num;
+            Console.WriteLine("Result: " + result);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input format");
+        }
+        finally
+        {
+            Console.WriteLine("Execution completed");
+        }
+    }
+}
