ad6df08abcda33779c46e98852897f202882392f
Kishorpatil007 <kp675541@gmail.com>
2026-04-10 19:33:49 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/124.COLLECTION ARRAYLISTS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList list = new ArrayList();
+        list.Add(10);
+        list.Add("Hello");
+        list.Add(25.5);
+        list.Add(true);
+
+        Console.WriteLine("ArrayList elements:");
+        foreach(var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        list.Remove("Hello");
+        Console.WriteLine("After removing 'Hello':");
+        foreach(var item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
