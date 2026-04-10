013406845c39ee431245e2f9b4b7024ae351d1b2
Kishorpatil007 <kp675541@gmail.com>
2026-04-10 19:32:13 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/123.THROWING EXCEPTION/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void ValidateNumber(int num)
+    {
+        if(num < 0)
+            throw new ArgumentException("Number cannot be negative");
+        else
+            Console.WriteLine("Valid number: " + num);
+    }
+
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            ValidateNumber(number);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Exception: " + ex.Message);
+        }
+    }
+}
