c3f90d8e9400b4086d89a820f6c9529cd20306ec
Kishorpatil007 <kp675541@gmail.com>
2026-04-10 19:34:45 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/125.COLLECTION-LIST/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>();
+        numbers.Add(10);
+        numbers.Add(20);
+        numbers.Add(30);
+
+        Console.WriteLine("List elements:");
+        foreach(int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
+        numbers.Remove(20);
+        Console.WriteLine("After removing 20:");
+        foreach(int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
