281d2158d4919a7c9f8a27b76b2e481ae4eb6cef
Kishorpatil007 <kp675541@gmail.com>
2026-04-10 19:31:13 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/122.TRY CATCH FINALLY/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int result = 100 / num;
+            Console.WriteLine("Result: " + result);
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine("Exception occurred: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Finally block executed");
+        }
+    }
+}
