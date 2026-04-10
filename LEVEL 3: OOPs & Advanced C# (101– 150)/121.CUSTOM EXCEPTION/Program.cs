c8198c02f8211e73fb374d24e05e12472413b84e
Kishorpatil007 <kp675541@gmail.com>
2026-04-10 19:30:18 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/121.CUSTOM EXCEPTION/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class InvalidAgeException : Exception
+{
+    public InvalidAgeException(string message) : base(message)
+    {
+    }
+}
+
+class Program
+{
+    static void CheckAge(int age)
+    {
+        if(age < 18)
+            throw new InvalidAgeException("Age must be 18 or older.");
+        else
+            Console.WriteLine("Age is valid: " + age);
+    }
+
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Enter age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            CheckAge(age);
+        }
+        catch(InvalidAgeException ex)
+        {
+            Console.WriteLine("Custom Exception: " + ex.Message);
+        }
+    }
+}
