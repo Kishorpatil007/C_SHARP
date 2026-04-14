5f8e2481b9204c40026c66070bb9278c91ca33c2
Kishorpatil007 <kp675541@gmail.com>
2026-04-14 16:03:39 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/130.DEMONSTRATE LINQ GROUPBY/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = { "Amit", "Anil", "Rahul", "Ravi", "Sneha", "Sita" };
+
+        var grouped = names.GroupBy(n => n[0]);
+
+        foreach (var group in grouped)
+        {
+            Console.WriteLine(group.Key);
+
+            foreach (var name in group)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
