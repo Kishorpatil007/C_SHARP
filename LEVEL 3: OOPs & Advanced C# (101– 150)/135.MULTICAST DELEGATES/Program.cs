075aa699216de4e9ab63a21127fef9813fba9b1e
Kishorpatil007 <kp675541@gmail.com>
2026-04-14 16:08:11 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/135.MULTICAST DELEGATES/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+delegate void Show();
+
+class Program
+{
+    static void Method1()
+    {
+        Console.WriteLine("Method1");
+    }
+
+    static void Method2()
+    {
+        Console.WriteLine("Method2");
+    }
+
+    static void Main()
+    {
+        Show s = Method1;
+        s += Method2;
+
+        s();
+    }
+}
