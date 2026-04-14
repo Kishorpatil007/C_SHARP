ffcfee119c1b91e3c18c0379272f916dfdf6b094
Kishorpatil007 <kp675541@gmail.com>
2026-04-14 16:07:18 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/134.DEMONSTRATE DELEGATES/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+delegate int Operation(int a, int b);
+
+class Program
+{
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void Main()
+    {
+        Operation op = Add;
+
+        Console.WriteLine(op(10, 5));
+    }
+}
