8b701307f0c4ce71babf30d8bb3b3b522e6fb197
Kishorpatil007 <kp675541@gmail.com>
2026-04-14 15:59:53 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/127.DEMONSTRATE STACK AND QUEUE/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Stack<int> stack = new Stack<int>();
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+
+        Console.WriteLine("Stack:");
+        foreach (int i in stack)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("Pop: " + stack.Pop());
+
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(100);
+        queue.Enqueue(200);
+        queue.Enqueue(300);
+
+        Console.WriteLine("Queue:");
+        foreach (int i in queue)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("Dequeue: " + queue.Dequeue());
+    }
+}
