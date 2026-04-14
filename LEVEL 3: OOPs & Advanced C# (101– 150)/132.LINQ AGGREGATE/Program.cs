ea329f9599b878bd6b169d9fc279b57de8df75b8
Kishorpatil007 <kp675541@gmail.com>
2026-04-14 16:05:31 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/132.LINQ AGGREGATE/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        int sum = numbers.Aggregate((a, b) => a + b);
+
+        Console.WriteLine(sum);
+    }
+}
