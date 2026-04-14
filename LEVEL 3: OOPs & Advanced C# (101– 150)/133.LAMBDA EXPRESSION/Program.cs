2526f628bbd02d57dc9301ccbabd33877373e027
Kishorpatil007 <kp675541@gmail.com>
2026-04-14 16:06:25 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/133.LAMBDA EXPRESSION/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Func<int, int, int> add = (a, b) => a + b;
+
+        Console.WriteLine(add(5, 3));
+
+        Func<int, int> square = x => x * x;
+
+        Console.WriteLine(square(4));
+    }
+}
