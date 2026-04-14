22124e75859acc041bc1251df9a97818c18db557
Kishorpatil007 <kp675541@gmail.com>
2026-04-14 16:09:04 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/136.DEMO EVENTS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Process
+{
+    public event Action OnProcessCompleted;
+
+    public void Start()
+    {
+        Console.WriteLine("Process Started");
+        OnProcessCompleted?.Invoke();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Process p = new Process();
+
+        p.OnProcessCompleted += () => Console.WriteLine("Process Completed");
+
+        p.Start();
+    }
+}
