2276368c6352f258636d1b08e1205c2281331229
Kishorpatil007 <kp675541@gmail.com>
2026-04-14 16:10:00 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/137.DEMONSTRATE FUNC,ACTION,PREDIACTE/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Func<int, int, int> add = (a, b) => a + b;
+        Console.WriteLine(add(2, 3));
+
+        Action<string> print = s => Console.WriteLine(s);
+        print("Hello");
+
+        Predicate<int> isEven = x => x % 2 == 0;
+        Console.WriteLine(isEven(4));
+    }
+}
