c5b12e0f00744cad9f092710c9fe9ace9d6640ab
Kishorpatil007 <kp675541@gmail.com>
2026-04-14 15:58:21 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/126.demonstrate dictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<int, string> students = new Dictionary<int, string>();
+
+        students.Add(1, "Amit");
+        students.Add(2, "Rahul");
+        students.Add(3, "Sneha");
+
+        foreach (var item in students)
+        {
+            Console.WriteLine(item.Key + " " + item.Value);
+        }
+
+        Console.WriteLine("Enter key to search:");
+        int key = int.Parse(Console.ReadLine());
+
+        if (students.ContainsKey(key))
+        {
+            Console.WriteLine(students[key]);
+        }
+        else
+        {
+            Console.WriteLine("Not Found");
+        }
+    }
+}
