290292eac9bb10d62c1d64de967a2809f745aec7
Kishorpatil007 <kp675541@gmail.com>
2026-04-14 16:01:20 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/128.DEMONSTARATE HASHSET/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        HashSet<int> numbers = new HashSet<int>();
+
+        numbers.Add(10);
+        numbers.Add(20);
+        numbers.Add(30);
+        numbers.Add(20);
+
+        foreach (int n in numbers)
+        {
+            Console.WriteLine(n);
+        }
+
+        Console.WriteLine("Enter number to check:");
+        int x = int.Parse(Console.ReadLine());
+
+        if (numbers.Contains(x))
+        {
+            Console.WriteLine("Exists");
+        }
+        else
+        {
+            Console.WriteLine("Does not exist");
+        }
+    }
+}
