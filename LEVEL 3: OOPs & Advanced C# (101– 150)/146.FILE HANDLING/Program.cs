544f3ae07b087232d9625d46f0255678b4ca1428
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:20:34 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/146.FILE HANDLING/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string path = "test.txt";
+
+        File.WriteAllText(path, "Hello File Handling");
+
+        string data = File.ReadAllText(path);
+
+        Console.WriteLine(data);
+    }
+}
