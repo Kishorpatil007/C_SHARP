281d8181f7c2854470cf6fef79a369ece784c38a
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:27:10 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/149.ATTRIBUTES/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Class)]
+class InfoAttribute : Attribute
+{
+    public string Name;
+
+    public InfoAttribute(string name)
+    {
+        Name = name;
+    }
+}
+
+[Info("Custom Attribute Example")]
+class Demo
+{
+}
+
+class Program
+{
+    static void Main()
+    {
+        Type t = typeof(Demo);
+        object[] attrs = t.GetCustomAttributes(false);
+
+        foreach (object attr in attrs)
+        {
+            InfoAttribute info = (InfoAttribute)attr;
+            Console.WriteLine(info.Name);
+        }
+    }
+}
