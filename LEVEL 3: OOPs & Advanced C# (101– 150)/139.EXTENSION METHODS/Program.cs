0265ae97e0588a41cce48567f3764653b8b8e2f7
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:14:14 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/139.EXTENSION METHODS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+static class MyExtension
+{
+    public static int Cube(this int x)
+    {
+        return x * x * x;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int num = 3;
+        Console.WriteLine(num.Cube());
+    }
+}
