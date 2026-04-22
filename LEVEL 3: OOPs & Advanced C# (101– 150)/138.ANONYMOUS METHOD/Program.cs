b5bd331a15992398739a42962596ff473f6704b6
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:13:20 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/138.ANONYMOUS METHOD/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    delegate void Show(int x);
+
+    static void Main()
+    {
+        Show s = delegate (int x)
+        {
+            Console.WriteLine(x * x);
+        };
+
+        s(5);
+    }
+}
