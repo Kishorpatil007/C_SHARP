980c48a66ae0f3eb3f9c836c57e1eff2a59ce58c
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:19:22 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/145.LOCKING AND SYNCHRONIZATION/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+class Counter
+{
+    private int value = 0;
+    private object lockObj = new object();
+
+    public void Increment()
+    {
+        lock (lockObj)
+        {
+            value++;
+        }
+    }
+
+    public void Show()
+    {
+        Console.WriteLine(value);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Counter c = new Counter();
+
+        Thread t1 = new Thread(() =>
+        {
+            for (int i = 0; i < 1000; i++) c.Increment();
+        });
+
+        Thread t2 = new Thread(() =>
+        {
+            for (int i = 0; i < 1000; i++) c.Increment();
+        });
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+
+        c.Show();
+    }
+}
