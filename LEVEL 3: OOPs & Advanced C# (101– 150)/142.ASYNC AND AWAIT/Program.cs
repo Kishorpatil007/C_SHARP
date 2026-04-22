f6e7c2da927b86df99b41bd761dfca560e930fba
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:16:40 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/142.ASYNC AND AWAIT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task<int> CalculateAsync(int a, int b)
+    {
+        await Task.Delay(1000);
+        return a + b;
+    }
+
+    static async Task Main()
+    {
+        int a = 5;
+        int b = 7;
+        int result = await CalculateAsync(a, b);
+        Console.WriteLine(result);
+    }
+}
