36340530b4c407de109818dc66c737a893ded60b
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:17:34 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/143.MULTITHREADING/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void PrintNumbers()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(500);
+        }
+    }
+
+    static void Main()
+    {
+        Thread t1 = new Thread(PrintNumbers);
+        Thread t2 = new Thread(PrintNumbers);
+
+        t1.Start();
+        t2.Start();
+
+        t1.Join();
+        t2.Join();
+    }
+}
