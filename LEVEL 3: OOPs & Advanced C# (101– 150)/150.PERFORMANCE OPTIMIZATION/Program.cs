ab3822667d8ad22253007a46d780d3189819247e
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:28:24 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/150.PERFORMANCE OPTIMIZATION/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Stopwatch sw = new Stopwatch();
+
+        sw.Start();
+        int sum = 0;
+        for (int i = 0; i < 1000000; i++)
+        {
+            sum += i;
+        }
+        sw.Stop();
+
+        Console.WriteLine(sum);
+        Console.WriteLine(sw.ElapsedMilliseconds);
+    }
+}
