b050d0b53771875083061700a757522a1bed95d7
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:25:10 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/147.SERIALIZATION/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+
+class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student { Id = 1, Name = "Kishor" };
+        string json = JsonSerializer.Serialize(s);
+        Console.WriteLine(json);
+
+        Student obj = JsonSerializer.Deserialize<Student>(json);
+        Console.WriteLine(obj.Name);
+    }
+}
