8579bdc12e1a43a7e5959bda1da3ec3a114699a4
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:26:02 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/148.REFLECTION/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+class Demo
+{
+    public void Show()
+    {
+        Console.WriteLine("Hello Reflection");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Type t = typeof(Demo);
+        object obj = Activator.CreateInstance(t);
+        MethodInfo m = t.GetMethod("Show");
+        m.Invoke(obj, null);
+    }
+}
