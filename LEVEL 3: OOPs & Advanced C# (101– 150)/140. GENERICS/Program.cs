3fca6bc67e48b7946130da3810febaa0c28910d0
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:24:20 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/140. GENERICS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class GenericClass<T>
+{
+    public T value;
+
+    public void Show()
+    {
+        Console.WriteLine(value);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        GenericClass<int> obj1 = new GenericClass<int>();
+        obj1.value = 10;
+        obj1.Show();
+
+        GenericClass<string> obj2 = new GenericClass<string>();
+        obj2.value = "Hello";
+        obj2.Show();
+    }
+}
