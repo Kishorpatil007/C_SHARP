9d14f53b66143cacc33f2931a12d63b9bd18747b
Kishorpatil007 <kp675541@gmail.com>
2026-04-22 21:15:51 +0530

    Add files via upload

---
+++ LEVEL 3: OOPs & Advanced C# (101– 150)/141.GENERIC CONSTRAINTS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Test<T> where T : class
+{
+    public void Show(T item)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Test<string> obj = new Test<string>();
+        obj.Show("Constraint Example");
+    }
+}
