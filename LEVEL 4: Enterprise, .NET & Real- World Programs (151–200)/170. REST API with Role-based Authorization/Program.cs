a5f00574212febd406bae8886df60a388c7c286d
Kishorpatil007 <kp675541@gmail.com>
2026-05-01 09:51:21 +0530

    Add files via upload

---
+++ LEVEL 4: Enterprise, .NET & Real- World Programs (151–200)/170. REST API with Role-based Authorization/Program.cs
@@ -0,0 +1,27 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Add Authentication
+builder.Services.AddAuthentication("Bearer")
+.AddJwtBearer("Bearer", options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes("MySecretKey12345"))
+    };
+});
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.Run();
