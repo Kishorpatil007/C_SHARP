2c7c7c625f0257ba2cc8b04541e0b537d7cac056
Kishorpatil007 <kp675541@gmail.com>
2026-05-01 09:49:26 +0530

    Add files via upload

---
+++ LEVEL 4: Enterprise, .NET & Real- World Programs (151–200)/168. REST API using ASP.NET Core (Basic)/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DemoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> GetStudents()
+        {
+            return new List<string> { "Ram", "Shyam", "Sita" };
+        }
+
+        [HttpGet("{id}")]
+        public string GetStudent(int id)
+        {
+            return "Student ID: " + id;
+        }
+    }
+}
